65c1047c5a6807b3a7ed1bde8c1c79883138ef8d
StanTheAwesomeMan <nightmare091105@gmail.com>
2024-08-28 22:29:23 +0200

    more guhhhh

--- private_dot_config/sway/executable_install-packages.sh
+++ private_dot_config/sway/executable_install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yay -S --needed elvish fuzzel kitty nano nwg-shell nwg-shell-config network-manager-applet dunst sway waybar swayidle playerctl light
+yay -S --needed elvish fuzzel kitty nano nwg-shell nwg-shell-config network-manager-applet dunst sway waybar swayidle playerctl light ranger
 
 sudo cp /home/$USER/.config/sway/screenshot /usr/bin/screenshot
